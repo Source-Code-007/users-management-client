a122773796bfb12c37f1e09a51e4239ee026cc01
Source-Code-007 <utsho926@gmail.com>
2023-05-08 09:52:37 +0600

    UI update

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -3,16 +3,19 @@ import { useLoaderData } from 'react-router-dom';
 const Users = () => {
     const users = useLoaderData()
     return (
-        <div className='min-h-screen flex justify-center items-center bg-gray-200 text-gray-700'>
-            <div className='grid grid-cols-3 gap-3'>
-                {
-                    users.map(user => {
-                        return <div key={user._id} className='shadow p-5'>
-                            <h2>{user.name}</h2>
-                            <h2>{user.number}</h2>
-                        </div>
-                    })
-                }
+        <div className='min-h-screen bg-gray-200 text-gray-700 py-12'>
+            <div className='max-w-7xl mx-6 lg:mx-auto'>
+                <h2 className='font-bold text-4xl text-center mb-5'>Users database</h2>
+                <div className='grid grid-cols-3 gap-3'>
+                    {
+                        users.map(user => {
+                            return <div key={user._id} className='shadow p-5 bg-slate-100'>
+                                <h2>name: {user.name}</h2>
+                                <h2>number: {user.number}</h2>
+                            </div>
+                        })
+                    }
+                </div>
             </div>
         </div>
     );
